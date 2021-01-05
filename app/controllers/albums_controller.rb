961078218bab8856b99aeccfccad2f399db694cc
jaimedavid01 <jaimedavid01@yahoo.com>
2021-01-05 10:59:57 -0500

    session login page

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,38 @@
 class AlbumsController < ApplicationController
 
         def index
-            @movies =  Movie.all
+            @albums =  Album.all
+
+
+            pop = albums.select {|album| album.genre == "Pop"}
+            hiphop = albums.select {|album| album.genre == "hiphop"}
+            comedy = albums.select {|album| album.genre == "Comedy"}
+            comedy = albums.select {|album| album.genre == "Comedy"}
+            comedy = albums.select {|album| album.genre == "Comedy"}
+
+            @album_categories = [
+            {category: "Action", albums: action}, 
+            {category: "Romance", albums: romance}, 
+            {category: "Comedy", albums: comedy}, 
+            ]
+  
+
+
         end
     
+
+        Pop
+        Hip-hop and Rap. ...
+        Rock. ...
+        Dance and Electronic music. ...
+        Latin music. ...
+        Indie and Alternative Rock. ...
+        Classical music. ...
+        K-Pop.
+
+
         def show
-            @movie = Movie.find(params[:id])
+            @album = album.find(params[:id])
         end
-        
+
 end
