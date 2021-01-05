d69d876346849716b7a363922901a0b98d9f8916
jaimedavid01 <jaimedavid01@yahoo.com>
2021-01-05 10:38:40 -0500

    structuring

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :albums
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  delete '/sessions/reset', to: 'sessions#reset_page_visits', as: 'page_reset'
+  delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
+  get '/sessions/new', to: 'sessions#new', as: 'new_session'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
 end
